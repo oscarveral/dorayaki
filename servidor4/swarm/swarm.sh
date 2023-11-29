66d06a4416a94ea9abdf81bd904b7363ec589e42
Oscar Vera López <oscar.veral@um.es>
2023-11-29 19:20:07 +0100

    Fix typo.

--- servidor4/swarm/swarm.sh
+++ servidor4/swarm/swarm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This scripts configures the docker swarm environment for a manager node.
-# Manager is expected to have only one network interface, eth0, with a IP address 
-# assigned by DHCP or set manually.
+# Manager is expected to have only one network interface, eth0, with a fixed IP 
+# address assigned by DHCP or set manually.
 
 systemctl restart docker
 
