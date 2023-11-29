25671632ba4ad905df3ade7b3942ff9b725346d8
Oscar Vera López <oscar.veral@um.es>
2023-11-29 16:38:06 +0100

    Prueba de script.

--- servidor4/servidor4.sh
+++ servidor4/servidor4.sh
@@ -20,7 +20,7 @@ apt update
 apt install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 echo WARNING! If there is an error on public key finding, do next steps manually.
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o --batch --yes /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o --yes /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
