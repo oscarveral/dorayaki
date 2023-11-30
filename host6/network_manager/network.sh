1aa380e626ebfa6621301690f6a6bccdb0375081
Oscar Vera López <oscar.veral@um.es>
2023-11-30 14:25:15 +0100

    Changed script to avoid output.

--- host6/network_manager/network.sh
+++ host6/network_manager/network.sh
@@ -14,17 +14,17 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Force disable MAC randomization.
-rm /etc/NetworkManager/conf.d/*
+rm /etc/NetworkManager/conf.d/* 2> /dev/null
 cp network.conf /etc/NetworkManager/conf.d/10-network.conf
 
 # Network configuration.
 systemctl stop NetworkManager
-rm /etc/NetworkManager/system-connections/*
+rm /etc/NetworkManager/system-connections/* 2> /dev/null
 systemctl start NetworkManager
 
 # Interfaz de red solo anfitrión para uso de SSH desde el anfitrión de la VM.
-nmcli con add type ethernet con-name eth0 ifname eth0 ipv4.method manual ip4 192.168.62.40/24
+nmcli con add type ethernet con-name eth0 ifname eth0 ipv4.method manual ip4 192.168.62.40/24 2> /dev/null
 # Interfaz para uso cotidiano por parte del usuario de la maquina.
-nmcli con add type ethernet con-name eth1 ifname eth1 ipv4.method auto
+nmcli con add type ethernet con-name eth1 ifname eth1 ipv4.method auto 2> /dev/null
 
 systemctl restart NetworkManager
