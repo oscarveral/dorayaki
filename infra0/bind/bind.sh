88e9a7bd07969b5a815626c3919fbe6f4c6ad43c
Oscar Vera López <oscar.veral@um.es>
2023-12-02 14:43:19 +0100

    Infraestructure server.

---
+++ infra0/bind/bind.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Configuración de DNS Bind9. Se ejecutará en un contenedor Docker.
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CURRENT_PATH="$(pwd)"
+
+if [ "$SCRIPT_PATH" != "$CURRENT_PATH" ]; then
+	echo ERROR! This script must be executed from the same directory it is located. 1>&2
+	exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo ERROR! Please run as root. 1>&2
+  exit 1
+fi
+
+rm /etc/bind/named.conf.options 2> /dev/null
+rm /etc/bind/named.conf.local 2> /dev/null
+
+mkdir -p /etc/bind 2> /dev/null
+
+cp named.conf.options /etc/bind/named.conf.options
+cp named.conf.local /etc/bind/named.conf.local
+
+mkdir -p /etc/bind/zones
+
+cp db.dorayaki.org /etc/bind/zones/db.dorayaki.org
+cp db.172.162 /etc/bind/zones/db.172.16.2
---
+++ infra0/infra0.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CURRENT_PATH="$(pwd)"
+
+if [ "$SCRIPT_PATH" != "$CURRENT_PATH" ]; then
+	echo ERROR! This script must be executed from the same directory it is located. 1>&2
+	exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo ERROR! Please run as root. 1>&2
+  exit 1
+fi
+
+cd utils
+./hostname.sh > /dev/null
+./packages.sh > /dev/null
+cd ..
+
+cd bind
+./bind.sh > /dev/null
+cd ..
+
+cd docker
+./docker.sh > /dev/null
+cd ..
+
+cd netplan
+./network.sh > /dev/null
+cd ..
+
+echo Script configuration finished successfully. 1>&2
