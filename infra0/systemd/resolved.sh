314a52d6bf2ce5385a1dda42d5fa9bdabe2def13
Oscar Vera López <oscar.veral@um.es>
2023-12-02 15:16:01 +0100

    Disabled Stub DNS listener of systemd-resolved to allow BIND9 execution.

---
+++ infra0/systemd/resolved.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$SCRIPT_PATH" != "$CURRENT_PATH" ]; then
+	echo ERROR! This script must be executed from the same directory it is located. 1>&2
+	exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo ERROR! Please run as root. 1>&2
+  exit 1
+fi
+
+# Uncomment DNSStubListener=no in /etc/systemd/resolved.conf
+sed -i 's/#DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf 2> /dev/null
