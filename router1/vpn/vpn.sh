e8ad08981d814b0413124ff3aeba9a8cc169f9da
Oscar Vera López <oscar.veral@um.es>
2023-12-05 10:55:54 +0100

    Change radius config.

--- router1/vpn/vpn.sh
+++ router1/vpn/vpn.sh
@@ -37,7 +37,7 @@ cp easy-rsa/pki/dh.pem keys/
 cd $CURRENT_PATH
 
 cp server.conf /etc/openvpn/
-cp radius.conf /etc/openvpn/
+cp radius.cnf /etc/openvpn/
 
 deluser --remove-home openvpn 2> /dev/null
 adduser --system --shell /usr/sbin/nologin --no-create-home --group openvpn
