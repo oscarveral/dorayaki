02346c44bf6518c0dc7e531b9540d8c8fb8bd259
Oscar Vera López <oscar.veral@um.es>
2023-12-06 21:18:35 +0100

    Change greenbone listen addr.

--- servidor9/greenbone/greenbone.sh
+++ servidor9/greenbone/greenbone.sh
@@ -428,7 +428,7 @@ Group=gvm
 RuntimeDirectory=gsad
 RuntimeDirectoryMode=2775
 PIDFile=/run/gsad/gsad.pid
-ExecStart=/usr/local/sbin/gsad --foreground --listen=127.0.0.1 --port=9392 --http-only
+ExecStart=/usr/local/sbin/gsad --foreground --listen=172.16.1.2 --port=9392 --http-only
 Restart=always
 TimeoutStopSec=10
 
