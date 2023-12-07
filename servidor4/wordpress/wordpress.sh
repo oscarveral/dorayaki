b360d7aebaa6a4becc6ce99709017be0156a1e88
Oscar Vera López <oscar.veral@um.es>
2023-12-07 18:26:57 +0100

    Wordpress initial.

---
+++ servidor4/wordpress/wordpress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CURRENT_PATH="$(pwd)"
+
+if [ "$SCRIPT_PATH" != "$CURRENT_PATH" ]; then
+	echo ERROR! This script must be executed from the same directory it is located. 1>&2
+	exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo ERROR! Please run as root. 1>&2
+  exit 1
+fi
+
+mkdir /etc/wordpress 2> /dev/null
+
+cp -r database /etc/wordpress
