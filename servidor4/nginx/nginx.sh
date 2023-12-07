bdc9529aab2a43f7e87c51ba0428c0b37ca6334e
Oscar Vera López <oscar.veral@um.es>
2023-12-07 22:47:34 +0100

    Try http basic page.

---
+++ servidor4/nginx/nginx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CURRENT_PATH="$(pwd)"
+
+if [ "$SCRIPT_PATH" != "$CURRENT_PATH" ]; then
+	echo ERROR! This script must be executed from the same directory it is located. 1>&2
+	exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo ERROR! Please run as root. 1>&2
+  exit 1
+fi
+
+mkdir /etc/nginx/ 2> /dev/null
+cp nginx.conf /etc/nginx/
+
+mkdir -p /var/www/http/ 2> /dev/null
+cp index.html /var/www/http/
