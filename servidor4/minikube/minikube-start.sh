0e207c9bb827dbb7ef24d0f2083d4ea09357aefa
Oscar Vera López <oscar.veral@um.es>
2023-12-07 23:38:28 +0100

    Try http basic page.

--- servidor4/minikube/minikube-start.sh
+++ servidor4/minikube/minikube-start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd /etc/kubernets-docker/
-
 minikube start --memory=1900 --driver=docker
 
 minikube addons enable metrics-server
 
 #nohup minikube dashboard 2> /dev/null &
 
-minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
+minikube kubectl -- apply -f config.yaml
 
 stop=""
 while [ "$stop" == "" ] 
