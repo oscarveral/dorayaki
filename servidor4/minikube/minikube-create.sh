9aa509b66b6e240539d9005ef6ce2f8b6e6aa3a6
Oscar Vera López <oscar.veral@um.es>
2023-12-08 18:07:16 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -8,7 +8,5 @@ nohup minikube dashboard 2> /dev/null &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
-minikube kubectl -- port-forward service/nginx-service 8080:30080
-
 #CLUSTER_IP=$(minikube ip)
 #sudo iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination $CLUSTER_IP:30080
