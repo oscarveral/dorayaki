1eaaacfaddd46f7c2c45a6751150f71c9ee29d67
Oscar Vera López <oscar.veral@um.es>
2023-12-08 22:24:44 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -15,4 +15,4 @@ while [ "$(minikube kubectl -- get pods | grep -c "Running")" != "3" ]; do
 	sleep 1
 done
 
-nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 443:443
+nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8443:443
