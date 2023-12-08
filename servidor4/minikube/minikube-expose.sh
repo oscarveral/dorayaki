8e36da20239827f8e182fa5364542570995e84c2
Oscar Vera López <oscar.veral@um.es>
2023-12-08 15:45:13 +0100

    Try http basic page.

--- servidor4/minikube/minikube-expose.sh
+++ servidor4/minikube/minikube-expose.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo ERROR! Please run as root. 1>&2
-  exit 1
-fi
-
 # Get external ip for nginx-service
 EXTERNAL_IP=$(minikube kubectl -- get svc nginx-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 # Add iptables rule to forward port 8080 to 8080 on external ip
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to-destination $EXTERNAL_IP
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to-destination $EXTERNAL_IP
