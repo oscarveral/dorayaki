ed98c5606842fd6beb827c8f1a262eb9c31eb9e3
Oscar Vera López <oscar.veral@um.es>
2023-12-08 18:33:31 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -8,4 +8,6 @@ nohup minikube dashboard &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
+minikube kubectl -- wait --for=condition=Ready pods --all
+
 nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8080:8080 &
