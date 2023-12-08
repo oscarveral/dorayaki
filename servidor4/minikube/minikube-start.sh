797ea6c5005102dc94500f7514739549bc612554
Oscar Vera López <oscar.veral@um.es>
2023-12-08 02:00:14 +0100

    Try http basic page.

--- servidor4/minikube/minikube-start.sh
+++ servidor4/minikube/minikube-start.sh
@@ -6,7 +6,7 @@ minikube addons enable metrics-server
 
 #nohup minikube dashboard 2> /dev/null &
 
-minikube kubectl -- apply -f config.yaml
+minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
 nohup minikube tunnel &
 
--- servidor4/minikube/minikube-stop.sh
+++ servidor4/minikube/minikube-stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd /etc/kubernets-docker/
-
 minikube delete --all 2> /dev/null
