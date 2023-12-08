12f3e7511a53dca11dd1435d25825066bee0517d
Oscar Vera López <oscar.veral@um.es>
2023-12-08 20:57:50 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -6,7 +6,7 @@ minikube addons enable metrics-server
 
 minikube image load nginx-custom
 
-nohup minikube dashboard &
+#nohup minikube dashboard &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
