02e320c78c6c4db116d40e4afa2f7231a8763057
Oscar Vera López <oscar.veral@um.es>
2023-12-08 15:33:24 +0100

    Try http basic page.

--- servidor4/minikube/minikube-expose.sh
+++ servidor4/minikube/minikube-expose.sh
@@ -4,4 +4,4 @@
 EXTERNAL_IP=$(minikube kubectl -- get svc nginx-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 # Add iptables rule to forward port 8080 to 8080 on external ip
-iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination $EXTERNAL_IP:8080
+iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 8080 -j DNAT --to-destination $EXTERNAL_IP:8080
