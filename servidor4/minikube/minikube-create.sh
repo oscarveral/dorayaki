789bbd1406372b486fca6d3a7d3c2cf8873af973
Oscar Vera López <oscar.veral@um.es>
2023-12-08 18:28:25 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -4,8 +4,8 @@ minikube start --memory=1900 --driver=docker
 
 minikube addons enable metrics-server
 
-nohup minikube dashboard 2> /dev/null &
+nohup minikube dashboard &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
-nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8080:8080 2> /dev/null &
+nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8080:8080 &
