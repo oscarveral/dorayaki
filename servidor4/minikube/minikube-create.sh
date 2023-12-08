130f538a3687517a96dc741e3dc2a4730816c017
Oscar Vera López <oscar.veral@um.es>
2023-12-08 18:24:53 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -8,6 +8,4 @@ nohup minikube dashboard 2> /dev/null &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
-nohup minikube kubectl -- port-forward service/nginx-service 8080:80 2> /dev/null &
-
-sudo iptables -t nat -A PREROUTING -p tcp --dport 8080 -j DNAT --to-destination 127.0.0.1:8080
+nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8080:80 2> /dev/null &
