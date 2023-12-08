ad3bb5f96c43c2155e0c6e4e3467e6f0fd1587eb
Oscar Vera López <oscar.veral@um.es>
2023-12-08 17:44:33 +0100

    Try http basic page.

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -7,3 +7,7 @@ minikube addons enable metrics-server
 nohup minikube dashboard 2> /dev/null &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
+
+CLUSTER_IP=$(minikube ip)
+
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to-destination $CLUSTER_IP:30080
--- servidor4/minikube/minikube-expose.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Get cluster instance ip
-CLUSTER_IP=$(minikube ip)
-
-# Route
