1a36b210114bdf2d834adfd67c7acf70329c6a80
Carlos Agulló Domingo <72665336+carlostriste@users.noreply.github.com>
2023-12-09 09:58:10 +0100

    Update default.sh

--- router1/iptables/default.sh
+++ router1/iptables/default.sh
@@ -32,50 +32,50 @@ iptables -P OUTPUT ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# SNAT. Hide internal networks behind eth2.
-iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
+# SNAT. Hide internal networks behind "$ISP".
+iptables -t nat -A POSTROUTING -o "$ISP" -j MASQUERADE
 
 # Allow related inbound traffic for all interfaces.
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Allow internal networks to access external networks and allow responses back in.
-iptables -A FORWARD -o eth2 -j ACCEPT
+iptables -A FORWARD -o "$ISP" -j ACCEPT
 iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-# DHCP. Only allow DHCP traffic from the internal networks of eth0 and eth1.
-iptables -A INPUT -i eth0 -p udp --dport 67:68 --sport 67:68 -j ACCEPT
-iptables -A INPUT -i eth1 -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+# DHCP. Only allow DHCP traffic from the internal networks of "$HOSTS" and "$SERVERS".
+iptables -A INPUT -i "$HOSTS" -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+iptables -A INPUT -i "$SERVERS" -p udp --dport 67:68 --sport 67:68 -j ACCEPT
 
 # OpenVPN. Only allow OpenVPN stablish traffic from outside the organization.
-iptables -A INPUT -i eth2 -p udp --dport 1194 -j ACCEPT
+iptables -A INPUT -i "$ISP" -p udp --dport 1194 -j ACCEPT
 
 # SSH. Restric only SSH from external network.
-iptables -A INPUT -i eth2 -p tcp --dport 22 -m state --state NEW -j REJECT --reject-with icmp-host-prohibited
+iptables -A INPUT -i "$ISP" -p tcp --dport 22 -m state --state NEW -j REJECT --reject-with icmp-host-prohibited
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A FORWARD -i eth2 -p tcp --dport 22 -m state --state NEW -j REJECT --reject-with icmp-host-prohibited
+iptables -A FORWARD -i "$ISP" -p tcp --dport 22 -m state --state NEW -j REJECT --reject-with icmp-host-prohibited
 iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
 
 # DNS. Allow DNS traffic only to DNS server and from DNS server to external networks. As this is a public service, DNAT is needed.
-iptables -t nat -A PREROUTING -i eth2 -p udp --dport 53 -j DNAT --to-destination 172.16.2.254
-iptables -A FORWARD -o eth1 -d 172.16.2.254 -p udp --dport 53 -j ACCEPT
+iptables -t nat -A PREROUTING -i "$ISP" -p udp --dport 53 -j DNAT --to-destination 172.16.2.254
+iptables -A FORWARD -o "$SERVERS" -d 172.16.2.254 -p udp --dport 53 -j ACCEPT
 
 # Radius. Allow requests only from servers LAN to internal server.
-iptables -A FORWARD -i eth1 -s 172.16.2.0/24 -p udp --dport 1812 -d 172.16.1.2 -j ACCEPT
+iptables -A FORWARD -i "$SERVERS" -s "$SERVERS_NET" -p udp --dport 1812 -d 172.16.1.2 -j ACCEPT
 
 # Greenbone. Allow access to web interface only to organization hosts. As only VPN clients are outside the organization, we need to allow access.
 # Physical hosts on office already have access to this service as they share LAN with the server.
-iptables -A FORWARD -i tun0 -o eth0 -s 172.16.1.0/24 -p tcp --dport 9392 -d 172.16.1.2 -j ACCEPT
+iptables -A FORWARD -i tun0 -o "$HOSTS" -s "$HOSTS_NET" -p tcp --dport 9392 -d 172.16.1.2 -j ACCEPT
 
 # Wordpress. Allow access to any host on the internet. As this is a public service, DNAT is needed.
-iptables -t nat -A PREROUTING -i eth2 -p tcp --dport 80 -j DNAT --to-destination 172.16.2.2
-iptables -A FORWARD -o eth1 -d 172.16.2.2 -p tcp --dport 80 -j ACCEPT
+iptables -t nat -A PREROUTING -i "$ISP" -p tcp --dport 80 -j DNAT --to-destination 172.16.2.2
+iptables -A FORWARD -o "$SERVERS" -d 172.16.2.2 -p tcp --dport 80 -j ACCEPT
 
 # Wordpress admin panel. Allow access to host on office only.
-iptables -A FORWARD -i eth0 -o eth1 -s 172.16.1.0/24 -p tcp --dport 9000 -d 172.16.2.2 -j ACCEPT
+iptables -A FORWARD -i "$HOSTS" -o "$SERVERS" -s "$HOSTS_NET" -p tcp --dport 9000 -d 172.16.2.2 -j ACCEPT
 
 # HTTPS Server. Allow requests HTTPS request only to this server. As this is a public service, DNAT is needed.
 iptables -t nat -A PREROUTING -p tcp --dport 8443 -j DNAT --to-destination 172.16.2.2
-iptables -A FORWARD -o eth1 -d 172.16.2.2 -p tcp --dport 8443 -j ACCEPT
+iptables -A FORWARD -o "$SERVERS" -d 172.16.2.2 -p tcp --dport 8443 -j ACCEPT
 
 # Docker Swarm. Is used only by servers LAN. Default rejection is applied.
 
