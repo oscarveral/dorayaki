f28ba2a7215caa786b41176f5693ed75554e9502
Oscar Vera López <oscar.veral@um.es>
2023-12-11 19:49:34 +0100

    VPN fix certs.

--- router1/vpn/genkeys.sh
+++ router1/vpn/genkeys.sh
@@ -25,14 +25,14 @@ echo "01" > serial
 cd /home/router1
 
 # CA
-openssl req -new -x509 -nodes -keyout vpnCA/CAkey/CA_key.pem -out vpnCA/CA_cert.pem -days 3650 -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=dorayaki.org/OU=VPN"
+openssl req -new -x509 -nodes -keyout vpnCA/CAkey/CA_key.pem -out vpnCA/CA_cert.pem -days 3650 -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=dorayaki.org/OU=VPN/emailAddress=admin@dorayaki.org"
 
 # Server
-openssl req -new -nodes -keyout vpnCA/Pkeys/vpn_pkey.pem -out vpnCA/reqs/vpn_req.pem -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=dorayaki.org/OU=VPN"
+openssl req -new -nodes -keyout vpnCA/Pkeys/vpn_pkey.pem -out vpnCA/reqs/vpn_req.pem -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=dorayaki.org/OU=VPN/emailAddress=admin@dorayaki.org"
 openssl ca -in vpnCA/reqs/vpn_req.pem -batch -out vpnCA/certs/vpn_cert.pem -config ./openssl.cnf
 
 # Client
-openssl req -new -nodes -keyout vpnCA/Pkeys/client_pkey.pem -out vpnCA/reqs/client_req.pem -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=client.dorayaki.org/OU=VPN"
+openssl req -new -nodes -keyout vpnCA/Pkeys/client_pkey.pem -out vpnCA/reqs/client_req.pem -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=client.dorayaki.org/OU=VPN/emailAddress=admin@dorayaki.org"
 openssl ca -in vpnCA/reqs/client_req.pem -batch -out vpnCA/certs/client_cert.pem -config ./openssl.cnf
 
 # Diffie-Hellman
