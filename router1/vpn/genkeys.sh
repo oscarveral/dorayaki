8ee246cfd2031104b922d7e74f84fe1fcb8cdfbe
Oscar Vera López <oscar.veral@um.es>
2023-12-11 19:42:54 +0100

    VPN fix certs.

--- router1/vpn/genkeys.sh
+++ router1/vpn/genkeys.sh
@@ -27,11 +27,11 @@ openssl req -new -x509 -nodes -keyout vpnCA/CAkey/CA_key.pem -out vpnCA/CA_cert.
 
 # Server
 openssl req -new -nodes -keyout vpnCA/Pkeys/vpn_pkey.pem -out vpnCA/reqs/vpn_req.pem -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=dorayaki.org"
-openssl ca -in vpnCA/reqs/vpn_req.pem -batch -out vpnCA/certs/vpn_cert.pem -config ./openssl.cnf
+#openssl ca -in vpnCA/reqs/vpn_req.pem -batch -out vpnCA/certs/vpn_cert.pem -config ./openssl.cnf
 
 # Client
 openssl req -new -nodes -keyout vpnCA/Pkeys/client_pkey.pem -out vpnCA/reqs/client_req.pem -config ./openssl.cnf -subj "/C=ES/ST=Murcia/L=Murcia/O=Dorayaki/CN=client.dorayaki.org"
-openssl ca -in vpnCA/reqs/client_req.pem -batch -out vpnCA/certs/client_cert.pem -config ./openssl.cnf
+#openssl ca -in vpnCA/reqs/client_req.pem -batch -out vpnCA/certs/client_cert.pem -config ./openssl.cnf
 
 # Diffie-Hellman
 openssl dhparam -out vpnCA/dh2048.pem 2048
