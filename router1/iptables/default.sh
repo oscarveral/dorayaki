4b3627c6162d626f2b905c6f88c927f650c3593b
Oscar Vera López <oscar.veral@um.es>
2023-12-12 16:58:57 +0100

    nagios conf.

--- router1/iptables/default.sh
+++ router1/iptables/default.sh
@@ -87,9 +87,8 @@ iptables -A INPUT ! -i "$ISP" -p tcp --dport 3000 -j ACCEPT
 # Nagios.
 iptables -A FORWARD -i tun0 -p tcp --dport 4000 -j ACCEPT
 
-# Ping
-iptables -A INPUT ! -i "$ISP" -p icmp --icmp-type time-exceeded -j ACCEPT
-iptables -A FORWARD ! -i "$ISP" -p icmp --icmp-type time-exceeded -j ACCEPT
+# Drop TRACEROUTE
+iptables -A OUTPUT -p icmp --icmp-type time-exceeded -j DROP
 
 # Docker Swarm. Is used only by servers LAN. Default rejection is applied.
 
