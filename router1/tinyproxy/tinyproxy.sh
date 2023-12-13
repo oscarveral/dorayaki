67fc602d695809e4893d85f2a8dd5e3953294cf2
carlostriste <carlos0105ad@gmail.com>
2023-12-13 10:32:16 +0100

    Tinyproxy

--- router1/tinyproxy/tinyproxy.sh
+++ router1/tinyproxy/tinyproxy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 CURRENT_PATH="$(pwd)"
 
@@ -13,17 +12,6 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-mkdir /etc/path
-mkdir /etc/path/to
-mkdir /etc/path/to/data
-mkdir /etc/path/to/main/
-mkdir /etc/path/to/main/config
 curl https://scripttiger.github.io/hosts-packages/hosts -o blacklist.txt
 
-#cp blacklist.txt /etc/path/to/
-cp squid.conf /etc/path/to/main/config
-
-rm blacklist.txt
-
 docker compose up -d
-
