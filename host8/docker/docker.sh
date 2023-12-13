32e1919755ef642ad6164b00392464301d951a59
Oscar Vera López <oscar.veral@um.es>
2023-12-14 00:26:27 +0100

    FINAL.

--- host8/docker/docker.sh
+++ host8/docker/docker.sh
@@ -16,25 +16,6 @@ systemctl enable docker --now
 
 usermod -aG docker $(logname)
 
-docker pull greenbone/vulnerability-tests
-docker pull greenbone/notus-data
-docker pull greenbone/scap-data
-docker pull greenbone/cert-bund-data
-docker pull greenbone/dfn-cert-data
-docker pull greenbone/data-objects
-docker pull greenbone/report-formats
-docker pull greenbone/gpg-data
-docker pull greenbone/redis-server
-docker pull greenbone/pg-gvm:stable
-docker pull greenbone/gvmd:stable
-docker pull greenbone/gsa:stable
-docker pull greenbone/ospd-openvas:stable
-docker pull greenbone/mqtt-broker
-docker pull greenbone/notus-scanner:stable
-docker pull greenbone/gvm-tools
+docker pull atomicorp/openvas
 
-docker rm -f $(docker ps -aq) 2> /dev/null
-
-cp compose.yaml /etc/docker/compose.yaml
-
-sudo -u $(logname) docker compose up -d
+docker run -d -p 443:443 -e OV_UPDATE=yes --name openvas atomicorp/openvas
