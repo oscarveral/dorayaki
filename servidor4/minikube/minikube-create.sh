a57cbac7f4c96ab0a71c9d589a609d83a942e613
Oscar Vera López <oscar.veral@um.es>
2023-12-14 19:42:36 +0100

    dashboard

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -6,7 +6,7 @@ minikube addons enable metrics-server
 
 minikube image load nginx-custom
 
-nohup minikube dashboard &
+nohup minikube dashboard --port 11111 &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
