99e18e2b8d8bc98deb58356ecb872f2230b6e5b8
Oscar Vera López <oscar.veral@um.es>
2023-12-14 19:19:36 +0100

    dashboard

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -3,10 +3,9 @@
 minikube start --memory=1900 --driver=docker
 
 minikube addons enable metrics-server
+minikube addons enable dashboard
 
 minikube image load nginx-custom
-
-#nohup minikube dashboard &
 
 minikube kubectl -- apply -f /etc/kubernets-docker/config.yaml
 
@@ -14,5 +13,10 @@ sleep 10
 while [ "$(minikube kubectl -- get pods | grep -c "Running")" != "3" ]; do
 	sleep 1
 done
+
+# Get container name of kubernetes-dashboard
+DASHBOARD_CONTAINER=$(minikube kubectl -- get pods | grep "kubernetes-dashboard" | awk '{print $1}')
+
+nohup minikube kubectl -- proxy --address 0.0.0.0 $DASHBOARD_CONTAINER 10000:80 --namespace=kubernetes-dashboard --disable-filter=true &
 
 nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8443:443
