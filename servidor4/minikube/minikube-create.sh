1fc3181c6ccde06f26297a6a278ceb37c9d1809d
Oscar Vera López <oscar.veral@um.es>
2023-12-14 19:27:26 +0100

    dashboard

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -14,6 +14,6 @@ while [ "$(minikube kubectl -- get pods | grep -c "Running")" != "3" ]; do
 	sleep 1
 done
 
-nohup minikube kubectl -- proxy --address 0.0.0.0 8001:80 --disable-filter=true &
+nohup minikube kubectl -- proxy --address 0.0.0.0 --disable-filter=true &
 
 nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8443:443
