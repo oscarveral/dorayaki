d034010b44aad0f9208490a75f986705807de3ed
Oscar Vera López <oscar.veral@um.es>
2023-12-14 19:27:06 +0100

    dashboard

--- servidor4/minikube/minikube-create.sh
+++ servidor4/minikube/minikube-create.sh
@@ -14,9 +14,6 @@ while [ "$(minikube kubectl -- get pods | grep -c "Running")" != "3" ]; do
 	sleep 1
 done
 
-# Get container name of kubernetes-dashboard
-DASHBOARD_CONTAINER=$(minikube kubectl -- get pods | grep "kubernetes-dashboard" | awk '{print $1}')
-
-nohup minikube kubectl -- proxy --address 0.0.0.0 $DASHBOARD_CONTAINER 10000:80 --namespace=kubernetes-dashboard --disable-filter=true &
+nohup minikube kubectl -- proxy --address 0.0.0.0 8001:80 --disable-filter=true &
 
 nohup minikube kubectl -- port-forward --address 0.0.0.0 service/nginx-service 8443:443
